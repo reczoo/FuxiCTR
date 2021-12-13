79b5dc953be48db3036f57df3d497e2f5d16558e
zhujiem <zhujiem@users.noreply.github.com>
2021-12-14 07:49:49 +0800

    Add copyright

--- fuxictr/pytorch/models/DCNv2.py
+++ fuxictr/pytorch/models/DCNv2.py
@@ -1,6 +1,7 @@
 # =========================================================================
 # Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
-# 
+# Copyright (C) 2021 The DeepCTR-Torch authors for CrossNetMix
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
