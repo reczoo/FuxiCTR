e3a9ffe0748cce514cbc6239827e43831012f166
xpai <xpai@users.noreply.github.com>
2024-06-21 20:08:17 +0800

    Add TransAct model

---
+++ model_zoo/TransAct/README.md
@@ -0,0 +1,4 @@
+# TransAct
+
+> Xue Xia, Pong Eksombatchai, Nikil Pancha, Dhruvil Deven Badani, Po-Wei Wang, Neng Gu, Saurabh Vishwas Joshi, Nazanin Farahpour, Zhiyuan Zhang, Andrew Zhai. [TransAct: Transformer-based Realtime User Action Model for Recommendation at Pinterest](https://arxiv.org/abs/2306.00248), in KDD 2023.
+
---
+++ model_zoo/TransAct/src/TransAct.py
@@ -0,0 +1,250 @@
+# =========================================================================
+# Copyright (C) 2024. The FuxiCTR Library. All rights reserved.
+# Copyright (C) 2023. TransAct Authors from Pinterest. Modified from their original implementation.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========================================================================
+
+
+""" This model implements the paper: Xia et al., TransAct: Transformer-based Realtime User Action
+    Model for Recommendation at Pinterest, KDD 2023.
+    [PDF] https://arxiv.org/abs/2306.00248
+    [Code] https://github.com/pinterest/transformer_user_action
+"""
+
+
+import torch
+from torch import nn
+import numpy as np
+import random
+from pandas.core.common import flatten
+from fuxictr.pytorch.models import BaseModel
+from fuxictr.pytorch.layers import FeatureEmbeddingDict, MLP_Block, CrossNetV2
+from torch.nn import MultiheadAttention
+
+
+class TransAct(BaseModel):
+    """
+    The TransAct model class that implements transformer-based realtime user action model.
+    Make sure the behavior sequences are sorted in chronological order and padded in the left part.
+
+    Args:
+        feature_map: A FeatureMap instance used to store feature specs (e.g., vocab_size).
+        model_id: Equivalent to model class name by default, which is used in config to determine 
+            which model to call.
+        gpu: gpu device used to load model.
+        hidden_activations: hidden activations used in MLP blocks (default="ReLU").
+        dcn_cross_layers: number of cross layers in DCNv2 (default=3).
+        dcn_hidden_units: hidden units of deep part in DCNv2 (default=[256, 128, 64]).
+        mlp_hidden_units: hidden units of MLP on top of DCNv2 (default=[]).
+        num_heads: number of heads of transformer (default=1).
+        transformer_layers: number of stacked transformer layers used in TransAct (default=1).
+        transformer_dropout: dropout rate used in transformer (default=0).
+        dim_feedforward: FFN dimension in transformer (default=512)
+        learning_rate: learning rate for training (default=1e-3).
+        embedding_dim: embedding dimension of features (default=64).
+        net_dropout: dropout rate for deep part in DCNv2 (default=0).
+        batch_norm: whether to apply batch normalization in DCNv2 (default=False).
+        target_item_field (List[tuple] or List[str]): which field is used for target item
+            embedding. When tuple is applied, the fields in each tuple are concatenated, e.g.,
+            item_id and cate_id can be concatenated as target item embedding.
+        sequence_item_field (List[tuple] or List[str]): which field is used for sequence item
+            embedding. When tuple is applied, the fields in each tuple are concatenated.
+        first_k_cols: number of hidden representations to pick as transformer output (default=1).
+        use_time_window_mask (Boolean): whether to use time window mask in TransAct (default=False).
+        time_window_ms: time window in ms to mask the most recent behaviors (default=86400000).
+        concat_max_pool (Boolean): whether cancate max pooling result in transformer output
+            (default=True).
+        embedding_regularizer: regularization term used for embedding parameters (default=0).
+        net_regularizer: regularization term used for network parameters (default=0).
+    """
+    def __init__(self,
+                 feature_map,
+                 model_id="TransAct",
+                 gpu=-1,
+                 hidden_activations="ReLU",
+                 dcn_cross_layers=3,
+                 dcn_hidden_units=[256, 128, 64],
+                 mlp_hidden_units=[],
+                 num_heads=1,
+                 transformer_layers=1,
+                 transformer_dropout=0,
+                 dim_feedforward=512,
+                 learning_rate=1e-3,
+                 embedding_dim=64,
+                 net_dropout=0,
+                 batch_norm=False,
+                 target_item_field=[("item_id", "cate_id")],
+                 sequence_item_field=[("click_history", "cate_history")],
+                 first_k_cols=1,
+                 use_time_window_mask=False,
+                 time_window_ms=86400000,
+                 concat_max_pool=True,
+                 embedding_regularizer=None,
+                 net_regularizer=None,
+                 **kwargs):
+        super().__init__(feature_map, 
+                         model_id=model_id, 
+                         gpu=gpu, 
+                         embedding_regularizer=embedding_regularizer, 
+                         net_regularizer=net_regularizer,
+                         **kwargs)
+        self.target_item_field = (
+            target_item_field if type(target_item_field) == list
+            else [target_item_field]
+        )
+        self.sequence_item_field = (
+            sequence_item_field if type(sequence_item_field) == list
+            else [sequence_item_field]
+        )
+        self.feature_map = feature_map
+        self.embedding_dim = embedding_dim
+        self.embedding_layer = FeatureEmbeddingDict(feature_map, embedding_dim)
+        self.transformer_encoders = nn.ModuleList()
+        seq_out_dim = 0
+        for sequence_field, item_field in zip(self.sequence_item_field, self.target_item_field):
+            seq_emb_dim = (
+                embedding_dim * len(sequence_field) if type(sequence_field) == tuple
+                else embedding_dim
+            )
+            target_emb_dim = (
+                embedding_dim * len(item_field) if type(item_field) == tuple
+                else embedding_dim
+            )
+            transformer_in_dim = seq_emb_dim + target_emb_dim
+            seq_out_dim += (first_k_cols + int(concat_max_pool)) * transformer_in_dim
+            self.transformer_encoders.append(
+                TransActTransformer(transformer_in_dim,
+                                    dim_feedforward=dim_feedforward,
+                                    num_heads=num_heads,
+                                    dropout=transformer_dropout,
+                                    transformer_layers=transformer_layers,
+                                    use_time_window_mask=use_time_window_mask,
+                                    time_window_ms=time_window_ms,
+                                    first_k_cols=first_k_cols,
+                                    concat_max_pool=concat_max_pool)
+            )
+        dcn_in_dim = feature_map.sum_emb_out_dim() + seq_out_dim - seq_emb_dim
+        self.crossnet = CrossNetV2(dcn_in_dim, dcn_cross_layers)
+        self.parallel_dnn = MLP_Block(input_dim=dcn_in_dim,
+                                      output_dim=None, # output hidden layer
+                                      hidden_units=dcn_hidden_units,
+                                      hidden_activations=hidden_activations,
+                                      output_activation=None,
+                                      dropout_rates=net_dropout,
+                                      batch_norm=batch_norm)
+        dcn_out_dim = dcn_in_dim + dcn_hidden_units[-1]
+        self.mlp = MLP_Block(input_dim=dcn_out_dim,
+                             output_dim=1,
+                             hidden_units=mlp_hidden_units,
+                             hidden_activations=hidden_activations,
+                             output_activation=self.output_activation)
+        self.compile(kwargs["optimizer"], kwargs["loss"], learning_rate)
+        self.reset_parameters()
+        self.model_to_device()
+
+    def forward(self, inputs):
+        X = self.get_inputs(inputs)
+        feature_emb_dict = self.embedding_layer(X)
+        for idx, (target_field, sequence_field) in enumerate(
+            zip(self.target_item_field, self.sequence_item_field)
+            ):
+            target_emb = self.concat_embedding(target_field, feature_emb_dict)
+            sequence_emb = self.concat_embedding(sequence_field, feature_emb_dict)
+            seq_field = list(flatten([sequence_field]))[0] # flatten nested list to pick the first field
+            padding_mask = (X[seq_field].long() == 0) # 1's for masked positions
+            transformer_out = self.transformer_encoders[idx](
+                target_emb, sequence_emb, mask=padding_mask
+            )
+            feature_emb_dict[f"transact_{idx}"] = transformer_out
+        for feat in flatten(self.sequence_item_field):
+            if self.feature_map.features[feat]["type"] == "sequence":
+                feature_emb_dict.pop(feat, None) # delete sequence features
+        dcn_in_emb = torch.cat(list(feature_emb_dict.values()), dim=-1)
+        cross_out = self.crossnet(dcn_in_emb)
+        dnn_out = self.parallel_dnn(dcn_in_emb)
+        y_pred = self.mlp(torch.cat([cross_out, dnn_out], dim=-1))
+        return_dict = {"y_pred": y_pred}
+        return return_dict
+
+    def concat_embedding(self, field, feature_emb_dict):
+        if type(field) == tuple:
+            emb_list = [feature_emb_dict[f] for f in field]
+            return torch.cat(emb_list, dim=-1)
+        else:
+            return feature_emb_dict[field]
+
+
+class TransActTransformer(nn.Module):
+    def __init__(self,
+                 transformer_in_dim,
+                 dim_feedforward=64,
+                 num_heads=1,
+                 dropout=0,
+                 transformer_layers=1,
+                 use_time_window_mask=False,
+                 time_window_ms=86400000, # recent 24h
+                 first_k_cols=1,
+                 concat_max_pool=True):
+        super(TransActTransformer, self).__init__()
+        self.use_time_window_mask = use_time_window_mask
+        self.time_window_ms = time_window_ms
+        self.concat_max_pool = concat_max_pool
+        self.first_k_cols = first_k_cols
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=transformer_in_dim,
+            nhead=num_heads,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+            batch_first=True,
+        )
+        self.transformer_encoder = nn.TransformerEncoder(
+            encoder_layer, num_layers=transformer_layers
+        )
+        if self.concat_max_pool:
+            self.out_linear = nn.Linear(transformer_in_dim, transformer_in_dim)
+
+    def forward(self, target_emb, sequence_emb, time_interval_seq=None, mask=None):
+        # concat action sequence emb with target emb
+        seq_len = sequence_emb.size(1)
+        concat_seq_emb = torch.cat([sequence_emb,
+                                    target_emb.unsqueeze(1).expand(-1, seq_len, -1)], dim=-1)
+        # get sequence mask (1's are masked)
+        key_padding_mask = mask
+        if self.use_time_window_mask and self.training:
+            rand_time_window_ms = random.randint(0, self.time_window_ms)
+            time_window_mask = (time_interval_seq < rand_time_window_ms)
+            key_padding_mask = torch.bitwise_or(key_padding_mask, time_window_mask)
+        tfmr_out = self.transformer_encoder(src=concat_seq_emb,
+                                            src_key_padding_mask=key_padding_mask)
+        tfmr_out = tfmr_out.masked_fill(
+            key_padding_mask.unsqueeze(-1).repeat(1, 1, tfmr_out.shape[-1]), 0.
+        )
+        # process the transformer output
+        output_concat = []
+        output_concat.append(tfmr_out[:, -self.first_k_cols:].flatten(start_dim=1))
+        if self.concat_max_pool:
+            # Apply max pooling to the transformer output
+            key_padding_mask = self.adjust_mask(key_padding_mask) # keep the last dim
+            tfmr_out = tfmr_out.masked_fill(
+                key_padding_mask.unsqueeze(-1).repeat(1, 1, tfmr_out.shape[-1]), -1e9
+            )
+            pooled_out = self.out_linear(tfmr_out.max(dim=1).values)
+            output_concat.append(pooled_out)
+        return torch.cat(output_concat, dim=-1)
+
+    def adjust_mask(self, mask):
+        # make sure not all actions in the sequence are masked
+        fully_masked = mask.all(dim=-1)
+        mask[fully_masked, -1] = 0
+        return mask
