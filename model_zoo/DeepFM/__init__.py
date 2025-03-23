a5bd12cbeb3d2d277f86768d4a224bc2b22c240f
FuatOgme <fuat.ogme@gmail.com>
2025-03-23 17:51:14 +0300

    added deepfm init

---
+++ model_zoo/DeepFM/__init__.py
@@ -0,0 +1,2 @@
+from .DeepFM_torch.src import DeepFM
+#from .DeepFM_torch.tf import DeepFM
--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 import setuptools
-import glob
-import os
 
 with open("README.md", "r", encoding="utf-8") as fd:
     long_description = fd.read()
-
-model_packages = [d.replace(os.path.sep, '.') for d in glob.glob('model_zoo/**/__init__.py', recursive=True)]
 
 setuptools.setup(
     name="fuxictr",
@@ -18,8 +14,6 @@ setuptools.setup(
     url="https://github.com/reczoo/FuxiCTR",
     download_url='https://github.com/reczoo/FuxiCTR/tags',
     packages=setuptools.find_packages(
-        where = ".",
-        include=["model_zoo"] + model_packages,  # Explicitly include model_zoo submodule
         exclude=["**/__pycache__"]
     ),
     include_package_data=True,
